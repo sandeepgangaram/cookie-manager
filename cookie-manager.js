d3556614efe179a75c3d9c5073c89f547139d896
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-08-27 10:38:57 +0530

    add set and get functions

--- cookie-manager.js
+++ cookie-manager.js
@@ -16,7 +16,56 @@
 (function (window, document) {
   "use strict";
 
-  var CookieManager = { test: "test" };
+  var CookieManager = {
+    /**
+     *
+     * Create and Set Cookie
+     * @param {String} name cookie name
+     * @param {String} value cookie value
+     * @param {Number} [expires] cookie expiration in days
+     * @param {String} [domain] cookie domain
+     * @param {String} [path] cookie path
+     * @param {Boolean} [secure] cookies ssl flag
+     */
+    set: function (name, value, expires, domain, path, secure) {
+      var cookieStr = name + "=" + value;
+
+      if (expires) {
+        var now = new Date();
+        now.setTime(now.getTime() + expires * 24 * 60 * 60 * 1000);
+        cookieStr += ";" + "expires=" + now.toUTCString();
+      }
+      if (domain) {
+        cookieStr += ";" + "domain=" + domain;
+      }
+      if (path) {
+        cookieStr += ";" + "path=" + path;
+      }
+      if (secure) {
+        cookieStr += ";" + "secure";
+      }
+
+      document.cookie = cookieStr;
+    },
+
+    /**
+     *
+     * Get Cookies by Name
+     * @param {String} name cookies name
+     *
+     */
+    get: function (name) {
+      var allCookies = document.cookie
+        .split(";")
+        .map((cookie) => cookie.trim().split("="));
+
+      var selectedCookie = allCookies.find((cookie) => cookie[0] === name);
+
+      if (selectedCookie != null) {
+        return selectedCookie[1];
+      }
+    },
+  };
 
   window.CookieManager = CookieManager;
 })(window, document);
