097c42ea1e724032894699c65c3af699dcbc5b14
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-08-27 08:08:29 +0530

    add library description

--- cookie-manager.js
+++ cookie-manager.js
@@ -1,1 +1,14 @@
-console.log("cookies-manager.js");
+/**
+ *
+ *
+ * cookie-manager.js
+ * simple, fast and lightweight cookies management library.
+ * 27-08-2023 - version 1.0
+ * {REPO URL}
+ *
+ * Copyright 2023 Sandeep Kumar Gangaram <gangaramsandeep@gmail.com>
+ * Release under MIT license
+ * {LICENSE URL}
+ *
+ *
+ */
