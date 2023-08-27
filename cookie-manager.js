51e4c75aaadedbcc4be6562c3289936e26a985e1
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-08-27 11:41:05 +0530

    add module support

--- cookie-manager.js
+++ cookie-manager.js
@@ -116,5 +116,20 @@
     },
   };
 
-  window.CookieManager = CookieManager;
+  // AMD support
+  if (typeof define === "function" && define.amd) {
+    define(function () {
+      return CookieManager;
+    });
+    // CommonJS and Node.js module support.
+  } else if (typeof exports !== "undefined") {
+    // Support Node.js specific `module.exports` (which can be a function)
+    if (typeof module !== "undefined" && module.exports) {
+      exports = module.exports = CookieManager;
+    }
+    // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+    exports.CookieManager = CookieManager;
+  } else {
+    window.CookieManager = CookieManager;
+  }
 })(window, document);
